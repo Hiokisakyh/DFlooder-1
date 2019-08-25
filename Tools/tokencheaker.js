bc354b70f8d0f3257e746e146da1d4dd024c8e61
Julian <33635418+xjulianCodes@users.noreply.github.com>
2019-08-26 02:36:13 +0300

    تعديل بسيط :P

--- Tools/tokencheaker.js
+++ Tools/tokencheaker.js
@@ -4,18 +4,15 @@ const fs = require("fs");
 const cache = JSON.parse(fs.readFileSync("./caches.json", "utf8"))
 const config = JSON.parse(fs.readFileSync("./tokens.json", "utf8"))
 const token = config.tokens;
-const printred = console.red;
-const printgreen = console.green;
-
-
-
+const printred = console.red; // wtf ??
+const printgreen = console.green; // wtf  ??
 
 client.on("ready", () => {
-printgreen("This Token Is Working Yay ! :)")
-process.exit()
-    })
+printgreen(`This Token Is Working Yay ! :) - ${cache.token}`);
+client.destroy();
+    });
 
 
 client.login(cache.token).catch(err => {
-    printred("This Token Is Not Working :( ")
+    printred(`This Token Is Not Working :( - ${cache.token}`);
 });
